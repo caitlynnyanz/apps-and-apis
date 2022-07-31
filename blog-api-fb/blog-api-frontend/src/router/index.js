9384c51e9b44c85ea3a34cab77cd9c2a5ea7faeb
Caitlynn Yanz <caitlynnyanz601@gmail.com>
2022-07-31 09:57:09 -0600

    added signup, login, and logout features for blog-api

--- blog-api-fb/blog-api-frontend/src/router/index.js
+++ blog-api-fb/blog-api-frontend/src/router/index.js
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import AboutView from "../views/AboutView.vue";
 import TestView from "../views/TestView.vue";
+import SignupView from "../views/SignupView.vue";
+import LoginView from "../views/LoginView.vue";
+import LogoutView from "../views/LogoutView.vue";
 
 const routes = [
   {
@@ -23,6 +26,9 @@ const routes = [
     name: "test",
     component: TestView,
   },
+  { path: "/signup", name: "signup", component: SignupView },
+  { path: "/login", name: "login", component: LoginView },
+  { path: "/logout", name: "logout", component: LogoutView },
 ];
 
 const router = createRouter({
